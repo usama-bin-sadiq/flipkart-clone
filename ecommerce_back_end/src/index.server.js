8b9b0f8975fb1854a54527583235df8fa37b24b8
Usama Bin Sadiq <usama.gormani8300@gmail.com>
2021-09-30 03:08:35 +0500

    Basic Server

---
+++ ecommerce_back_end/src/index.server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on Port ${process.env.PORT}`);
+})
